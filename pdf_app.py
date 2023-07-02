73f2e2e6c1b62b84dee504c53290b2ff86560c41
mille055 <46400739+mille055@users.noreply.github.com>
2023-07-02 14:38:03 -0400

    Update pdf_app.py

--- pdf_app.py
+++ pdf_app.py
@@ -10,3 +10,9 @@ from langchain.agents import create_csv_agent
 from langchain.llms import OpenAI
 from langchain.document_loaders import TextLoader
 from langchain.indexes import VectorstoreIndexCreator
+
+api_key = os.ENVIRON('OPENAI_API_KEY)
+if api_key is not None:
+  print('I have an openai api key')
+
+                     
