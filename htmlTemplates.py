450a0006d5a3cdfacfd55de1911762e68473536e
mille055 <46400739+mille055@users.noreply.github.com>
2023-07-04 14:29:57 -0400

    Update htmlTemplates.py

--- htmlTemplates.py
+++ htmlTemplates.py
@@ -37,7 +37,7 @@ bot_template = '''
 user_template = '''
 <div class="chat-message user">
     <div class="avatar">
-        <img src="https://github.com/mille055/explore_langchain/tree/main/assets/duke.png">
+        <img src="https://github.com/mille055/explore_langchain/blob/main/assets/duke.png">
     </div>    
     <div class="message">{{MSG}}</div>
 </div>
